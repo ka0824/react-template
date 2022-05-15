f65b4af129f6bbbc61942b3f1cb8355018118df3
ka0824 <rkgud0824@gmail.com>
2022-05-15 12:29:20 +0900

    feat[Select]: make select box

---
+++ src/fakeData/dataSelectOps.js
@@ -0,0 +1,16 @@
+const dataSelectOps = [
+  {
+    id: 0,
+    text: "Test1",
+  },
+  {
+    id: 1,
+    text: "Test2",
+  },
+  {
+    id: 2,
+    text: "Test3",
+  },
+];
+
+export default dataSelectOps;
